08cf2c50ec3ca59ea6d0170c514c304052c5d69d
vancedbrown <vbrown@smithfield.com>
2024-05-03 11:14:37 -0400

    Update from RStudio

--- semenorders.R
+++ semenorders.R
@@ -11,40 +11,40 @@ orders1<-orders[c(3,5,7,9,10,11,12:15,18,19:33,35)]
 
 hs<- createStyle(textDecoration = "BOLD")
 
-aarmas<-orders1 %>% filter(Specialist=="ARMAS TORRES, ANDRES ANTONIO")
+aarmas<-orders1 %>% filter(Specialist=="ARMAS TORRES, ANDRES A")
 write.xlsx(aarmas,"ANDRES_ARMAS.xlsx", colWidths="auto", headerStyle = hs)
 
 barmstrong<-orders1 %>% filter(Specialist=="ARMSTRONG, BARRY N")
 write.xlsx(barmstrong,"NEIL_ARMSTRONG.xlsx", colWidths="auto", headerStyle = hs)
 
-jautry<-orders1 %>% filter(Specialist=="AUTRY, JEREMY W.")
+jautry<-orders1 %>% filter(Specialist=="AUTRY, JEREMY W")
 write.xlsx(jautry,"JEREMY_AUTRY.xlsx", colWidths="auto", headerStyle = hs)
 
 jbaldwin<-orders1 %>% filter(Specialist=="BALDWIN, JASON D")
 write.xlsx(jbaldwin,"JASON_BALDWIN.xlsx", colWidths="auto", headerStyle = hs)
 
-sbereza<-orders1 %>% filter(Specialist=="BEREZA, SEBASTIAN R.")
+sbereza<-orders1 %>% filter(Specialist=="BEREZA, SEBASTIAN R")
 write.xlsx(sbereza,"SEBASTIAN_BEREZA.xlsx", colWidths="auto", headerStyle = hs)
 
-mbryant<-orders1 %>% filter(Specialist=="BRYANT, MARVIN W.")
+mbryant<-orders1 %>% filter(Specialist=="BRYANT, MARVIN W")
 write.xlsx(mbryant,"MARVIN_BRYANT.xlsx", colWidths="auto", headerStyle = hs)
 
-rcollins<-orders1 %>% filter(Specialist=="COLLINS, RICKY D.")
+rcollins<-orders1 %>% filter(Specialist=="COLLINS, RICKY D")
 write.xlsx(rcollins,"RICKY_COLLINS.xlsx", colWidths="auto", headerStyle = hs)
 
-jgainey<-orders1 %>% filter(Specialist=="GAINEY, JAMES T.")
+jgainey<-orders1 %>% filter(Specialist=="GAINEY, JAMES T")
 write.xlsx(jgainey,"JAMES_GAINEY.xlsx", colWidths="auto", headerStyle = hs)
 
-rgarcia<-orders1 %>% filter(Specialist=="GARCIA RIVERA, RIGOBERTO DESIDERIO")
+rgarcia<-orders1 %>% filter(Specialist=="GARCIA RIVERA, RIGOBERTO D")
 write.xlsx(rgarcia,"RIGOBERTO_GARCIA.xlsx", colWidths="auto", headerStyle = hs)
 
-rholland<-orders1 %>% filter(Specialist=="HOLLAND, ROBERT G.")
+rholland<-orders1 %>% filter(Specialist=="HOLLAND, ROBERT G")
 write.xlsx(rholland,"ROBERT_HOLLAND.xlsx", colWidths="auto", headerStyle = hs)
 
-cholt<-orders1 %>% filter(Specialist=="HOLT, CHRISTOPHER M.")
+cholt<-orders1 %>% filter(Specialist=="HOLT, CHRISTOPHER M")
 write.xlsx(cholt,"CHRISTOPHER_HOLT.xlsx", colWidths="auto", headerStyle = hs)
 
-rjolly<-orders1 %>% filter(Specialist=="JOLLY, RANDY S.")
+rjolly<-orders1 %>% filter(Specialist=="JOLLY, RANDY S")
 write.xlsx(rjolly,"RANDY_JOLLY.xlsx", colWidths="auto", headerStyle = hs)
 
 sjozefowicz<-orders1 %>% filter(Specialist=="JOZEFOWICZ, SLAWOMIR")
@@ -53,7 +53,7 @@ write.xlsx(sjozefowicz,"SLAWOMIR_JOZEFOWICZ.xlsx", colWidths="auto", headerStyle
 jmauney<-orders1 %>% filter(Specialist=='MAUNEY, JAMES M')
 write.xlsx(jmauney,"JIMMY_MAUNEY.xlsx", colWidths="auto", headerStyle = hs)
 
-mmehlenbacher<-orders1 %>% filter(Specialist=="MEHLENBACHER, MATTHEW L.")
+mmehlenbacher<-orders1 %>% filter(Specialist=="MEHLENBACHER, MATTHEW L")
 write.xlsx(mmehlenbacher,"MATTHEW_MEHLENBACHER.xlsx", colWidths="auto", headerStyle = hs)
 
 dmurray<-orders1 %>% filter(Specialist=="MURRAY, DEBRA B.")
@@ -62,22 +62,22 @@ write.xlsx(dmurray,"DEBRA_MURRAY.xlsx", colWidths="auto", headerStyle = hs)
 mneill<-orders1 %>% filter(Specialist=="NEILL, MICHAEL T")
 write.xlsx(mneill,"MICHAEL_NEILL.xlsx", colWidths="auto", headerStyle = hs)
 
-rosorto<-orders1 %>% filter(Specialist=="OSORTO PERDOMO, ROGER ANTONIO")
+rosorto<-orders1 %>% filter(Specialist=="OSORTO PERDOMO, ROGER A")
 write.xlsx(rosorto,"ROGER_ANTONIO_OSORTO.xlsx", colWidths="auto", headerStyle = hs)
 
-lparr<-orders1 %>% filter(Specialist=="PARR, LAURA A.")
+lparr<-orders1 %>% filter(Specialist=="PARR, LAURA A")
 write.xlsx(lparr,"LAURA_PARR.xlsx", colWidths="auto", headerStyle = hs)
 
-jrouse<-orders1 %>% filter(Specialist=="ROUSE, JASON L.")
+jrouse<-orders1 %>% filter(Specialist=="ROUSE, JASON L")
 write.xlsx(jrouse,"JASON_ROUSE.xlsx", colWidths="auto", headerStyle = hs)
 
-wscott<-orders1 %>% filter(Specialist=="SCOTT, WILLIAM R.")
+wscott<-orders1 %>% filter(Specialist=="SCOTT, WILLIAM R")
 write.xlsx(wscott,"RAY_SCOTT.xlsx", colWidths="auto", headerStyle = hs)
 
-jtyer<-orders1 %>% filter(Specialist=="TYER, JOHN W.")
+jtyer<-orders1 %>% filter(Specialist=="TYER, JOHN W")
 write.xlsx(jtyer,"JOHN_TYER.xlsx", colWidths="auto", headerStyle = hs)
 
-wupchurch<-orders1 %>% filter(Specialist=="UPCHURCH, WILLIAM L.")
+wupchurch<-orders1 %>% filter(Specialist=="UPCHURCH, WILLIAM L")
 write.xlsx(wupchurch,"WILLIAM_UPCHURCH.xlsx", colWidths="auto", headerStyle = hs)
 
 bwarren<-orders1 %>% filter(Specialist=="WARREN, BAILEY A.")
@@ -86,7 +86,7 @@ write.xlsx(bwarren,"BAILEY_WARREN.xlsx", colWidths="auto", headerStyle = hs)
 ewatts<-orders1 %>% filter(Specialist=="WATTS, ERIC L")
 write.xlsx(ewatts,"ERIC_WATTS.xlsx", colWidths="auto", headerStyle = hs)
 
-jworley<-orders1 %>% filter(Specialist=="WORLEY, JEFFERY D.")
+jworley<-orders1 %>% filter(Specialist=="WORLEY, JEFFERY D")
 write.xlsx(jworley,"JEFF_WORLEY.xlsx", colWidths="auto", headerStyle = hs)
 
 
